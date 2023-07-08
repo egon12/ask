a6aaf9f9b9efd85c66679b9d03742bd56dd8b771
Egon Firman <egon.firman@tokopedia.com>
2023-07-08 16:43:20 +0700

    fix the binary name

--- src/simple_chatgpt/__main__.py
+++ src/simple_chatgpt/__main__.py
@@ -2,10 +2,17 @@ import os
 import sys
 from simple_chatgpt.ask import ask
 
-def main(args):
-  prompt = ' '.join(args)
-  res = ask(prompt)
-  print(res)
+def main():
+  args = sys.argv[1:]
+
+  if len(args) == 0:
+    prompt = input('Are there anything that I can help? ')
+  elif len(args) == 1 and args[0] == "-":
+    prompt = sys.stdin.read()
+  else:
+    prompt = ' '.join(args)
+
+  print(ask(prompt))
 
 if __name__ == "__main__":
-  main(sys.argv[1:])
+  main()
