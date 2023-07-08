4dd51dab8d1013776b7a62523845cf4018f4a755
Egon Firman <egon.firman@tokopedia.com>
2023-07-08 21:12:39 +0700

    Update version number to 0.0.2 and publish to test PyPI
    * Update `pyproject.toml` to set the version number to 0.0.2.
    * Update `src/simple_chatgpt/ask.py` to use Bard API instead of OpenAI API.

--- src/simple_chatgpt/ask.py
+++ src/simple_chatgpt/ask.py
@@ -1,12 +1,21 @@
 import openai
+import bardapi
 
 def ask(prompt):
-  if prompt == "":
-    return "Please ask a question!"
+  if prompt == '':
+    return 'Please ask a question!'
 
+  return ask_bard(prompt)
+  #return ask_chatgpt(prompt)
+
+def ask_bard(prompt):
+  answer = bardapi.core.Bard().get_answer(prompt)
+  return answer['content']
+
+def ask_chatgpt(prompt):
   res = openai.ChatCompletion.create(
-      model="gpt-3.5-turbo",
-      messages=[{"role":"user","content":prompt}],
+      model='gpt-3.5-turbo',
+      messages=[{'role':'user','content':prompt}],
       temperature=1,
       max_tokens=512,
       top_p=1,
