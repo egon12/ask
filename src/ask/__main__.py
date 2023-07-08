fb5fa0a59d0d71becfc9d04a6d01fa571869818d
Egon Firman <egon.firman@tokopedia.com>
2023-07-08 15:08:00 +0700

    use new model

--- src/ask/__main__.py
+++ src/ask/__main__.py
@@ -3,27 +3,20 @@ import os
 import sys
 
 def main():
-    prompt = ' '.join(sys.argv[1:])
-    res = ask(prompt)
-    print(res.choices[0].text)
-
-def ask_from_file(filename):
-    f = open(filename)
-    prompt = f.read()
-    res = ask(prompt)
-    print(res.choices[0].text)
+  prompt = ' '.join(sys.argv[1:])
+  res = ask(prompt)
+  print(res.choices[0].message.content)
 
 def ask(prompt):
-    return openai.Completion.create(
-      model="text-davinci-003",
-      prompt=prompt + "\n",
-      temperature=0.7,
+  return openai.ChatCompletion.create(
+      model="gpt-3.5-turbo",
+      messages=[{"role":"user","content":prompt}],
+      temperature=1,
       max_tokens=256,
       top_p=1,
-      frequency_penalty=0.0,
-      presence_penalty=0.0,
-    )
+      frequency_penalty=0,
+      presence_penalty=0
+      )
 
 if __name__ == "__main__":
-    main()
-    #ask_from_file("diff.txt")
+  main()
