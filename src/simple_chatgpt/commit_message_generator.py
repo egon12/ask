1c5d0e23ab52a9b79b38c16598c48a03cc451cbc
Egon Firman <egon.firman@tokopedia.com>
2023-07-09 16:00:11 +0700

    Add commit message generator functionality to the project

---
+++ src/simple_chatgpt/commit_message_generator.py
@@ -0,0 +1,13 @@
+from git import Git
+from ask import ask
+
+commit_message_prompt = 'Please generate commit message based on this diff\n\n'
+
+def generate_commit_message():
+    git = Git()
+    diff = git.diff('--staged')
+    commit_message = ask(commit_message_prompt + diff)
+    return commit_message
+
+if __name__ == '__main__':
+    print(generate_commit_message())
